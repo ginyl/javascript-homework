39b0f2b47367a99f719950663c9c32760f3d86d4
Ying Li <kaitlyn130.yl@gmail.com>
2016-08-09 17:32:10 -0400

    js alert to object

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//this is a basic js alert
+alert('Hello World');
+
+//to test if the code works
+console.log("hello");
+console.log(2+2);
+
+//variables can be set to represent a string denoted by "" or numbers
+var name = "Gough";
+console.log(name);
+
+//an array can consisted of different types of data such as strings and numbers
+//an array is denoted by []
+//the first item in an array is identified as 0
+var myFriends = ["John", 8, 23, 34, 65];
+var list = ["soccer", "fencing", "boxing"];
+console.log(myFriends[0]);
+console.log(myFriends, list);
+
+//an object is denoted by {}
+var car = {
+	make: "Toyota",
+	model: "Prius"
+};
+//dot notation and bracket notation
+console.log(car.model);
+console.log(car['make']);
+
